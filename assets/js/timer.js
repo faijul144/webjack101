05dea1143aadc50713cb1af58e0186294ddf1e2d
Faijul Islam Fuad <f.faijulislam144@gmail.com>
2021-08-24 16:17:17 +0600

    All elements added v68

---
+++ assets/js/timer.js
@@ -0,0 +1,52 @@
+/*=====================
+  timer js
+ ==========================*/
+
+"use strict";
+
+function makeMeTwoDigits(n) {
+  return (n < 10 ? "0" : "") + n;
+}
+
+$(document).ready(function () {
+  //    Set the date we're counting down to
+  var countDownDate = new Date("Sep 5, 2021 15:37:25").getTime();
+
+  //    Update the count down every 1 second
+  var x = setInterval(function () {
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("countdownTime").innerHTML =
+      "<span>" +
+      makeMeTwoDigits(days) +
+      " Days</span>, " +
+      "<span>" +
+      makeMeTwoDigits(hours) +
+      "<span class='pl-1'>:</span> </span>" +
+      "<span>" +
+      makeMeTwoDigits(minutes) +
+      "<span class='pl-1'>:</span> </span>" +
+      "<span>" +
+      makeMeTwoDigits(seconds) +
+      "</span> ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdownTime").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+});
